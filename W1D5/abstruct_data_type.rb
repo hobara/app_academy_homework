73d713d1b03ce0c61c2bcb1cdb887aeec7996fc5
Hiro Obara <hobara1211@gmail.com>
2017-06-01 23:27:05 -0700

    updated

---
+++ W1D5/abstruct_data_type.rb
@@ -0,0 +1,76 @@
+class Stack
+
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack << el
+    el
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack.dup
+  end
+
+end
+
+
+class Queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+    el
+  end
+
+  def dequeue
+    @stack.pop
+  end
+
+  def show
+    @queue.dup
+  end
+
+end
+
+
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    key_matched = @map.select { |arr| arr[0] == key }
+    if key_matched.empty?
+      @map << [key, value]
+    else
+      idx = @map.index(key_matched[0])
+      @map[idx] = [key, value]
+    end
+  end
+
+  def lookup(key)
+    key_matched = @map.select { |arr| arr[0] == key }
+    key_matched.empty? ? nil : key_matched.first
+  end
+
+  def remove(key)
+    @map.delete(lookup(key))
+  end
+
+  def show
+    copy = []
+    @map.each { |arr| copy << arr.dup }
+    copy
+  end
+
+end
